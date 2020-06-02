85200f750e49c04dddce57c863dd9ba5724016b1
Jung Lee <lee.jung.s95@gmail.com>
2020-06-02 04:45:02 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,12 +4,13 @@ class Backer
 
   attr_accessor :name, :backed_projects
 
+  @@backed_projects = []
   def initialize(name)
     @name = name
     @backed_projects = []
   end
 
   def back_project(project)
-    
+
   end
 end
